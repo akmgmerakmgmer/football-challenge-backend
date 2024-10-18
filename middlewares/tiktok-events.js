332c28c005b2df7bcab3bc883ec4e33c19a2c4a4
akmgmerakmgmer <ahmedever80@gmail.com>
2024-10-18 21:55:20 +0300

    New

--- middlewares/tiktok-events.js
+++ middlewares/tiktok-events.js
@@ -18,11 +18,42 @@ const handler = (eventName, next) => {
     next()
 }
 const loginHandler = (req, res, next) => {
-    handler('Click button', next)
+    const payload = {
+        pixel_code: '<CS99IHJC77UFDI751310>', // Replace with your actual TikTok Pixel Code
+        event: 'Click button', // This could be "Purchase", "CompleteRegistration", etc.
+        test_event_code: 'TEST08223', // Test event code from TikTok Events API
+    };
+
+
+    // Send the POST request to TikTok's Events API
+    const response = fetch('https://business-api.tiktok.com/v1.2/pixel/track/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Token': '<46bda4d67232e02d0c42f074f8289c6e4dac5106>', // Replace with TikTok Access Token
+        },
+        body: JSON.stringify(payload),
+    });
+    next()
 
 }
 const signupHandler = (req, res, next) => {
-    handler('Complete Registration', next)
+    const payload = {
+        pixel_code: '<CS99IHJC77UFDI751310>', // Replace with your actual TikTok Pixel Code
+        event: 'Complete Registration', // This could be "Purchase", "CompleteRegistration", etc.
+        test_event_code: 'TEST08223', // Test event code from TikTok Events API
+    };
 
+
+    // Send the POST request to TikTok's Events API
+    const response = fetch('https://business-api.tiktok.com/v1.2/pixel/track/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Token': '<46bda4d67232e02d0c42f074f8289c6e4dac5106>', // Replace with TikTok Access Token
+        },
+        body: JSON.stringify(payload),
+    });
+    next()
 }
 export default { loginHandler, signupHandler }
