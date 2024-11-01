c81843c8d96178dbe245a9bbed22db2f12f66d57
akmgmerakmgmer <ahmedever80@gmail.com>
2024-11-01 19:57:40 +0200

    New

--- routes/api.js
+++ routes/api.js
@@ -41,7 +41,7 @@ router.put('/buy-perks/:id', onlyUserAuth, usersController.buy_perks)
 router.put('/notify-about/:id', onlyUserAuth, usersController.notify_about)
 
 //Question Routes
-router.post('/questions', onlyAdminAuth, questionsController.create_questions)
+router.post('/questions', questionsController.create_questions)
 router.get('/questions', requireAuth, questionsController.get_questions)
 router.get('/admin-questions', onlyAdminAuth, questionsController.get_admin_questions)
 router.get('/questions/:id', onlyAdminAuth, questionsController.get_single_question)
