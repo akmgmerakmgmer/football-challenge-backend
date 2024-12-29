a2fa40175eea3bacb0aa78f5afc4728b0f527ea4
akmgmerakmgmer <ahmedever80@gmail.com>
2024-12-29 22:26:02 +0200

    New

--- models/rankModel.js
+++ models/rankModel.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const RankModel = new Schema({
+    bgImage: {
+        type: String,
+        required: [true, 'field_required']
+    },
     image: {
         type: String,
         required: [true, 'field_required']
